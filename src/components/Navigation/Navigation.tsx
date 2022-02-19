79ad9f9fc2770beed25c120df13e92c49ab0b148
supminn <30731236+supminn@users.noreply.github.com>
2022-02-19 13:18:08 +0530

    style: color schemes selected

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,3 +1,16 @@
+import { Link } from "react-router-dom";
+
 export const Navigation = () => {
-  return <>Navigation</>;
+  return (
+    <>
+      <Link to="/">
+        <i className="fa-regular fa-face-awesome"></i>
+        <span>SupForum</span>
+      </Link>
+      <section>
+        <Link to="/login">Login</Link>
+        <Link to="/singup">Signup</Link>
+      </section>
+    </>
+  );
 };
